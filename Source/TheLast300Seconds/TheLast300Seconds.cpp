6ad2229cbbcaae045b59eb4830ae4a7ea3c7fb1a
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2025-06-02 01:05:39 +0200

    Initial commit
    - Created and configured the C++ settings of the project
    - Made the GitHub repository

---
+++ Source/TheLast300Seconds/TheLast300Seconds.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheLast300Seconds.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheLast300Seconds, "TheLast300Seconds" );
